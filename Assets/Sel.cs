750eb47fed788bf5cf64aa81642d062336af437d
klh0076 <64940560+klh0076@users.noreply.github.com>
2021-03-22 15:05:54 -0700

    Add files via upload

---
+++ Assets/Sel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Sel : MonoBehaviour
+{
+    public Button b;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    public void Pressed()
+    {
+        S.clicked(b.transform.name);
+
+    
+    }
+}
